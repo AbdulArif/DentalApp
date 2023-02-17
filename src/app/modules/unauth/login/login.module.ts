ee7ca9aedfb1f3e89c7614adbe744ab0f621d778
AbdulArif <abdularif.dgp@gmail.com>
2023-02-17 18:08:38 +0530

    update

--- src/app/modules/unauth/login/login.module.ts
+++ src/app/modules/unauth/login/login.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
