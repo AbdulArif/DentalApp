f01baa5d4f5afafdce19849936f42e055fd4a24d
AbdulArif <abdularif.dgp@gmail.com>
2024-11-02 21:54:06 +0530

    prescription update

--- src/app/modules/auth/prescription/prescription.component.ts
+++ src/app/modules/auth/prescription/prescription.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-prescription',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prescription.component.scss']
 })
 export class PrescriptionComponent implements OnInit {
+  @ViewChild('parentdiv', { static: false }) el!: ElementRef
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  downloadPdf() {
+    const pdf = new jsPDF();
+    var elementHTML = <HTMLElement>document.querySelector("#parentdiv");
+    pdf.html(elementHTML, {
+      callback: () => {
+        var pageCount = pdf.internal.pages.length
+        for (let i = pageCount; i >= 2; i--) {
+          pdf.deletePage(i)
+        }
+        pdf.save("sample.pdf");
+      },
+      x: 0,
+      y: 0,
+      width: 210,
+      windowWidth: elementHTML.offsetWidth
+    })
   }
 
 }
--- src/app/modules/unauth/global-login/global-login.component.ts
+++ src/app/modules/unauth/global-login/global-login.component.ts
@@ -10,30 +10,10 @@ import "jspdf/dist/polyfills.es.js";
 })
 export class GlobalLoginComponent implements OnInit {
 
-  @ViewChild('parentdiv', { static: false }) el!: ElementRef
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-
-  downloadPdf() {
-    const pdf = new jsPDF();
-    var elementHTML = <HTMLElement>document.querySelector("#parentdiv");
-    pdf.html(elementHTML, {
-      callback: () => {
-        var pageCount = pdf.internal.pages.length
-        for (let i = pageCount; i >= 2; i--) {
-          pdf.deletePage(i)
-        }
-        pdf.save("sample.pdf");
-      },
-      x: 0,
-      y: 0,
-      width: 210,
-      windowWidth: elementHTML.offsetWidth
-    })
   }
 
 }
