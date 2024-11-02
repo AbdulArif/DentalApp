4a7ce6a3a498b3ae4f23dd895e89fda8dc88237e
kabirulraj <kabirul2001@gmail.com>
2024-11-02 18:24:03 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://ironmanapi.azurewebsites.net"
+  // apiUrl: "https://ironmanapi.azurewebsites.net"
+  apiUrl: "http://dentalcaree.runasp.net/"
+
 };
