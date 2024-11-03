92ef03f30ec05081f819f70276811b7ae9273e5e
AbdulArif <abdularif.dgp@gmail.com>
2024-11-03 18:51:56 +0530

    User title

--- src/app/modules/auth/user/user.component.ts
+++ src/app/modules/auth/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-user',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private title: Title,
+
+  ) { }
 
   ngOnInit(): void {
+    this.title.setTitle("User")
   }
 
 }
