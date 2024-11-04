15c3bcdc1a37e681d79ba842795eb932efb30f11
kabirulraj <kabirul2001@gmail.com>
2024-11-04 21:01:32 +0530

    update api url cloud

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: "https://ironmanapi.azurewebsites.net"
-  apiUrl: "http://dentalcaree.runasp.net/"
+  apiUrl: "dentalapi-a7c8cxepfrcea5hy.westeurope-01.azurewebsites.net"
 
 };
