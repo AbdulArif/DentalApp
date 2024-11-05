bade511e8dcda5d5134ad7e91b92b3886c6172db
AbdulArif <abdularif.dgp@gmail.com>
2024-11-05 20:13:00 +0530

    update golobal login

--- src/app/modules/unauth/global-login/global-login.component.ts
+++ src/app/modules/unauth/global-login/global-login.component.ts
@@ -1,6 +1,13 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { jsPDF } from "jspdf";
 import "jspdf/dist/polyfills.es.js";
+import { ToastrService } from 'ngx-toastr';
+import { first } from 'rxjs';
+import { AuthenticationService } from 'src/app/services/core/authentication.service';
+import { FieldToggleService } from 'src/app/services/shared/field-toggle.service';
+import { WindowNavigatorService } from 'src/app/services/shared/window-navigator.service';
 
 
 @Component({
@@ -10,10 +17,58 @@ import "jspdf/dist/polyfills.es.js";
 })
 export class GlobalLoginComponent implements OnInit {
 
+  loginForm!: UntypedFormGroup;
+  loginForm_loading: boolean = false
 
-  constructor() { }
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    private authenticationService: AuthenticationService,
+    private toastr: ToastrService,
+    private router: Router,
+    public tieldToggleService: FieldToggleService,
+    private windowNavigatorService: WindowNavigatorService
+  ) { }
 
   ngOnInit(): void {
+    this.buildLoginForm();
+  }
+  get f() { return this.loginForm.controls; }
+
+  buildLoginForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(15)]],
+      userAgent: [this.windowNavigatorService.userAgent, Validators.required],
+      loginDateTime: null
+    });
   }
 
+  toggleFieldTextType() {
+    this.tieldToggleService.toggleField();
+  }
+
+  onSubmit(): void {
+    this.loginForm_loading = true;
+    if (this.loginForm.invalid) {
+      this.loginForm_loading = true;
+      this.toastr.warning('Fill all the required fields!.', 'Warning', { positionClass: 'toast-bottom-right', closeButton: true, progressBar: true, progressAnimation: 'decreasing' });
+      return;
+    }
+    this.loginForm.patchValue({
+      loginDateTime: new Date().toISOString()
+    })
+    console.log(this.loginForm.value)
+    this.authenticationService.globalLogin(this.loginForm.value).pipe(first()).subscribe(
+      {
+        next: (data) => {
+          this.loginForm_loading = false;
+          this.router.navigate(['auth']);
+        },
+        error: (error) => {
+          this.loginForm_loading = false;
+          this.toastr.error(error.error, 'Error', { positionClass: 'toast-bottom-right', closeButton: true, progressBar: true, progressAnimation: 'decreasing' });
+        }
+      }
+    );
+  }
 }
