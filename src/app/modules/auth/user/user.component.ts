deaf615fc1e7536e1a23398ccd54d7ebd386e966
AbdulArif <abdularif.dgp@gmail.com>
2024-11-29 18:57:34 +0530

    update user

--- src/app/modules/auth/user/user.component.ts
+++ src/app/modules/auth/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 import { ToastrService } from 'ngx-toastr';
 import { MenuItem } from 'primeng/api';
@@ -8,6 +8,7 @@ import { Role } from 'src/app/models/account/account.model';
 import { AccountService } from 'src/app/services/account/account.service';
 import { UserService } from 'src/app/services/account/user.service';
 import { AuthenticationService } from 'src/app/services/core/authentication.service';
+import { MenuService } from 'src/app/services/menu/menu.service';
 import { FieldToggleService } from 'src/app/services/shared/field-toggle.service';
 
 
@@ -21,201 +22,12 @@ export class UserComponent implements OnInit {
   userId!: string
   userName!: string
   items!: MenuItem[];
-  products = [
-    {
-      id: '1000',
-      code: 'f230fh0g3',
-      name: 'Bamboo Watch',
-      description: 'Product Description',
-      image: 'bamboo-watch.jpg',
-      price: 65,
-      category: 'Accessories',
-      quantity: 24,
-      inventoryStatus: 'INSTOCK',
-      rating: 5
-    },
-    {
-      id: '1001',
-      code: 'nvklal433',
-      name: 'Black Watch',
-      description: 'Product Description',
-      image: 'black-watch.jpg',
-      price: 72,
-      category: 'Accessories',
-      quantity: 61,
-      inventoryStatus: 'OUTOFSTOCK',
-      rating: 4
-    },
-    {
-      id: '1002',
-      code: 'zz21cz3c1',
-      name: 'Blue Band',
-      description: 'Product Description',
-      image: 'blue-band.jpg',
-      price: 79,
-      category: 'Fitness',
-      quantity: 2,
-      inventoryStatus: 'LOWSTOCK',
-      rating: 3
-    },
-    {
-      id: '1003',
-      code: '244wgerg2',
-      name: 'Blue T-Shirt',
-      description: 'Product Description',
-      image: 'blue-t-shirt.jpg',
-      price: 29,
-      category: 'Clothing',
-      quantity: 25,
-      inventoryStatus: 'INSTOCK',
-      rating: 5
-    },
-    {
-      id: '1004',
-      code: 'h456wer53',
-      name: 'Bracelet',
-      description: 'Product Description',
-      image: 'bracelet.jpg',
-      price: 15,
-      category: 'Accessories',
-      quantity: 73,
-      inventoryStatus: 'INSTOCK',
-      rating: 4
-    },
-    {
-      id: '1005',
-      code: 'av2231fwg',
-      name: 'Brown Purse',
-      description: 'Product Description',
-      image: 'brown-purse.jpg',
-      price: 120,
-      category: 'Accessories',
-      quantity: 0,
-      inventoryStatus: 'OUTOFSTOCK',
-      rating: 4
-    },
-    {
-      id: '1006',
-      code: 'bib36pfvm',
-      name: 'Chakra Bracelet',
-      description: 'Product Description',
-      image: 'chakra-bracelet.jpg',
-      price: 32,
-      category: 'Accessories',
-      quantity: 5,
-      inventoryStatus: 'LOWSTOCK',
-      rating: 3
-    },
-    {
-      id: '1007',
-      code: 'mbvjkgip5',
-      name: 'Galaxy Earrings',
-      description: 'Product Description',
-      image: 'galaxy-earrings.jpg',
-      price: 34,
-      category: 'Accessories',
-      quantity: 23,
-      inventoryStatus: 'INSTOCK',
-      rating: 5
-    },
-    {
-      id: '1008',
-      code: 'vbb124btr',
-      name: 'Game Controller',
-      description: 'Product Description',
-      image: 'game-controller.jpg',
-      price: 99,
-      category: 'Electronics',
-      quantity: 2,
-      inventoryStatus: 'LOWSTOCK',
-      rating: 4
-    },
-    {
-      id: '1009',
-      code: 'cm230f032',
-      name: 'Gaming Set',
-      description: 'Product Description',
-      image: 'gaming-set.jpg',
-      price: 299,
-      category: 'Electronics',
-      quantity: 63,
-      inventoryStatus: 'INSTOCK',
-      rating: 3
-    },
-    {
-      id: '1010',
-      code: 'plb34234v',
-      name: 'Gold Phone Case',
-      description: 'Product Description',
-      image: 'gold-phone-case.jpg',
-      price: 24,
-      category: 'Accessories',
-      quantity: 0,
-      inventoryStatus: 'OUTOFSTOCK',
-      rating: 4
-    },
-    {
-      id: '1011',
-      code: '4920nnc2d',
-      name: 'Green Earbuds',
-      description: 'Product Description',
-      image: 'green-earbuds.jpg',
-      price: 89,
-      category: 'Electronics',
-      quantity: 23,
-      inventoryStatus: 'INSTOCK',
-      rating: 4
-    },
-    {
-      id: '1012',
-      code: '250vm23cc',
-      name: 'Green T-Shirt',
-      description: 'Product Description',
-      image: 'green-t-shirt.jpg',
-      price: 49,
-      category: 'Clothing',
-      quantity: 74,
-      inventoryStatus: 'INSTOCK',
-      rating: 5
-    },
-    {
-      id: '1013',
-      code: 'fldsmn31b',
-      name: 'Grey T-Shirt',
-      description: 'Product Description',
-      image: 'grey-t-shirt.jpg',
-      price: 48,
-      category: 'Clothing',
-      quantity: 0,
-      inventoryStatus: 'OUTOFSTOCK',
-      rating: 3
-    },
-    {
-      id: '1014',
-      code: 'waas1x2as',
-      name: 'Headphones',
-      description: 'Product Description',
-      image: 'headphones.jpg',
-      price: 175,
-      category: 'Electronics',
-      quantity: 8,
-      inventoryStatus: 'LOWSTOCK',
-      rating: 5
-    },
-    {
-      id: '1015',
-      code: 'vb34btbg5',
-      name: 'Light Green T-Shirt',
-      description: 'Product Description',
-      image: 'light-green-t-shirt.jpg',
-      price: 49,
-      category: 'Clothing',
-      quantity: 34,
-      inventoryStatus: 'INSTOCK',
-      rating: 4
-    },
 
-  ];
+  selectedUser: any
+
+  createUserForm!: UntypedFormGroup
+  createUserForm_loading: boolean = false;
+  todaysDate: Date = new Date();
 
   constructor(
     private title: Title,
@@ -225,7 +37,7 @@ export class UserComponent implements OnInit {
     private authenticationService: AuthenticationService,
     private accountService: AccountService,
     private userService: UserService,
-    // private menuService: MenuService,
+    private menuService: MenuService,
   ) { }
 
   ngOnInit(): void {
@@ -233,7 +45,7 @@ export class UserComponent implements OnInit {
     this.clinicId = this.authenticationService.clinicId()
     this.userId = this.authenticationService.currentUserId()
     this.userName = this.authenticationService.currentUserFirstName() + ' ' + this.authenticationService.currentUserLastName();
-    // this.GetRoles();
+    this.GetRoles();
     this.GetMyEmployees();
     this.GetAvailableMenus()
     this.items = [
@@ -257,23 +69,23 @@ export class UserComponent implements OnInit {
     }
   }
 
-  // roles: Role[] = [];
-  // GetRoles(): void {
-  //   this.getRolesSub = this.accountService.GetRoles().subscribe(
-  //     {
-  //       next: (response: any) => {
-  //         console.log("Roles : ",response)
+  roles: Role[] = [];
+  GetRoles(): void {
+    this.getRolesSub = this.accountService.GetRoles().subscribe(
+      {
+        next: (response: any) => {
+          console.log("Roles : ", response)
 
-  //         this.roles = response;
-  //       },
-  //       error: (error: any) => {
-  //         this.toastr.error('Failed to get roles.', 'Error', { positionClass: 'toast-bottom-right', closeButton: true, progressBar: true, progressAnimation: 'decreasing' });
-  //       },
-  //       complete: () => {
-  //       }
-  //     }
-  //   );
-  // }
+          this.roles = response;
+        },
+        error: (error: any) => {
+          this.toastr.error('Failed to get roles.', 'Error', { positionClass: 'toast-bottom-right', closeButton: true, progressBar: true, progressAnimation: 'decreasing' });
+        },
+        complete: () => {
+        }
+      }
+    );
+  }
   myEmployees_loading: boolean = false
   myEmployees: any[] = [];
 
@@ -282,7 +94,7 @@ export class UserComponent implements OnInit {
     this.getEmployeeSub = this.userService.GetMyEmployees(this.clinicId, this.userId, this.authenticationService.currentUserRole()).subscribe(
       {
         next: (response: any) => {
-          console.log("GetMyEmployees result :",response)
+          console.log("GetMyEmployees result :", response)
           this.myEmployees = response;
           this.myEmployees_loading = false
         },
@@ -322,5 +134,17 @@ export class UserComponent implements OnInit {
     //   },
     //   error: (err) => { }
     // })
+  }
+  display: boolean = false
+  enablePass: boolean = false
+  
+  showDialog() {
+    this.display = true;
+    if (this.createUserForm.get('userId')?.value) {
+      this.enablePass = false
+    }
+    else {
+      this.enablePass = true
+    }
   }
 }
